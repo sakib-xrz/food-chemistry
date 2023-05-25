6ff6a779c7a8a0dc55113d44e1634455773677e6
sakib-xrz <sakibxrz21@gmail.com>
2023-05-25 17:18:38 +0600

    feat: Overview Component complete

--- src/lib/modules/home/Home.jsx
+++ src/lib/modules/home/Home.jsx
@@ -1,16 +1,29 @@
 import Banner from "../../components/Banner";
 import Categories from "../../components/Categories";
+import OverviewBanner from "../../components/OverviewBanner";
 import Title from "../../components/Title";
+import service from "../../../assets/home/chef-service.jpg";
+import Wrapper from "../../components/Wrapper";
 
 const Home = () => {
     return (
-        <div className="space-y-20">
+        <div className="space-y-14 lg:space-y-20">
             <Banner />
             <Title
                 title={"ORDER ONLINE"}
                 subtitle={"From 11:00am to 10:00pm"}
             />
             <Categories />
+            <Wrapper>
+                <OverviewBanner
+                    img={service}
+                    title={"FOOD CHEMISTRY"}
+                    description={
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo."
+                    }
+                    bgcolor={"bg-white"}
+                />
+            </Wrapper>
             <div></div>
         </div>
     );
