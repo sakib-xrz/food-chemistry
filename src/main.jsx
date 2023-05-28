ca943d23dbffcbdb98a1cc8aaf206093b9132313
sakib-xrz <sakibxrz21@gmail.com>
2023-05-28 15:34:46 +0600

    feat: menucover component complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
-import router from './lib/router/route';
+import router from './lib/router/Route';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
